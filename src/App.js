9af338add27eec2539a05ee1a53d1f3eb606b05f
Imrajkumar29 <rajkumar291994@gmail.com>
2022-11-27 15:09:59 +0530

    Welcome Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome
+        name="ravi"
+        pic="https://static.generated.photos/vue-static/home/feed/latino-male.png"
+      />
+      <Welcome
+        name="Karthik"
+        pic="https://i.dailymail.co.uk/i/pix/2013/08/29/article-2405475-1B8389EE000005DC-718_634x550.jpg"
+      />
+      <Welcome
+        name="karthik"
+        pic="https://pbs.twimg.com/media/D8dDZukXUAAXLdY.jpg"
+      />
     </div>
   );
 }
-
+//Component Declaration -> View + Logic
+function Welcome({ name, pic }) {
+  //let name = 'Chandru';
+  //console.log(props);
+  //const pic =
+  // "https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/35af6a41332353.57a1ce913e889.jpg";
+  return (<div>
+    <img className='user-pic' src={pic}></img>
+    <h1>
+      Hi, {name} -😁😁200 followers
+    </h1>
+  </div>)
+}
 export default App;
